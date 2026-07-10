5ca817a09a66a5e7879592ef6338320640e868d6
esneider25 <esneiderbencomo@gmail.com>
2026-07-10 13:26:36 -0400

    Update components.js

--- js/components.js
+++ js/components.js
@@ -7,7 +7,7 @@ function renderNavbar() {
     <nav class="navbar" id="navbar">
       <div class="navbar-inner">
         <div class="logo" onclick="navigateTo('home')">
-          <span class="logo-icon">🦈</span>
+          <img src="img/logo.png" alt="Logo" style="height: 38px; width: auto; object-fit: contain; filter: drop-shadow(0 0 8px var(--accent-glow));">
           <span class="logo-text">Recarga<span>Shark</span></span>
         </div>
         
